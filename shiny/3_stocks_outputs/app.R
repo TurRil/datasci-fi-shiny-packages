91b97f32236d2f5a2b2bdffcf18d4d91e1a9be5c
Jon Calder <jonmcalder@gmail.com>
2017-09-06 09:13:32 +0200

    Update shiny
    Remove unnecessary files in shiny stocks 1 and 2
    Swap app scripts for 3 and 4

--- shiny/3_stocks_outputs/app.R
+++ shiny/3_stocks_outputs/app.R
@@ -49,43 +49,26 @@ load("stock_data.RData")
 # Define server logic required to plot stocks
 server <- function(input, output) {
   
-  stock_filtered_data <- reactive({
-    stock_data[[input$stock]]
-  })
-  
-  date_filtered_data <- reactive({
-    stock_filtered_data() %>% 
-      filter(between(index, input$dates[1], input$dates[2])) %>% 
-      mutate(
-        group = case_when(
-          grepl("Volume", series) ~ "Volume",
-                            TRUE  ~ "Price"
-        )
-      )
-  })
-  
-  log_filtered_data <- reactive({
-    if (input$log) {
-      date_filtered_data() %>% 
-        mutate(value = log(value))
-    }
-    else {
-      date_filtered_data()
-    }
-  })
-  
   output$stock_price_plot <- renderPlot({
-    ggplot(
-      log_filtered_data() %>% filter(group == "Price"), 
-      aes(x = index, y = value, color = series)
-    ) + geom_line()
+    
+    price_data <- stock_data[[input$stock]] %>% 
+      filter(
+        between(index, input$dates[1], input$dates[2]),
+        !grepl("Volume", series)
+      )
+    
+    ggplot(price_data, aes(x = index, y = value, color = series)) + geom_line()
   })
   
   output$stock_volume_plot <- renderPlot({
-      ggplot(
-        log_filtered_data() %>% filter(group == "Volume"), 
-        aes(x = index, y = value, fill = series)
-      ) + geom_col()
+    
+    volume_data <- stock_data[[input$stock]] %>% 
+      filter(
+        between(index, input$dates[1], input$dates[2]),
+        grepl("Volume", series)
+      )
+      
+    ggplot(volume_data, aes(x = index, y = value, fill = series)) + geom_col()
   })
   
 }
--- shiny/4_stocks_reactivity/app.R
+++ shiny/4_stocks_reactivity/app.R
@@ -49,26 +49,43 @@ load("stock_data.RData")
 # Define server logic required to plot stocks
 server <- function(input, output) {
   
-  output$stock_price_plot <- renderPlot({
-    
-    price_data <- stock_data[[input$stock]] %>% 
-      filter(
-        between(index, input$dates[1], input$dates[2]),
-        !grepl("Volume", series)
+  stock_filtered_data <- reactive({
+    stock_data[[input$stock]]
+  })
+  
+  date_filtered_data <- reactive({
+    stock_filtered_data() %>% 
+      filter(between(index, input$dates[1], input$dates[2])) %>% 
+      mutate(
+        group = case_when(
+          grepl("Volume", series) ~ "Volume",
+                            TRUE  ~ "Price"
+        )
       )
-    
-    ggplot(price_data, aes(x = index, y = value, color = series)) + geom_line()
+  })
+  
+  log_filtered_data <- reactive({
+    if (input$log) {
+      date_filtered_data() %>% 
+        mutate(value = log(value))
+    }
+    else {
+      date_filtered_data()
+    }
+  })
+  
+  output$stock_price_plot <- renderPlot({
+    ggplot(
+      log_filtered_data() %>% filter(group == "Price"), 
+      aes(x = index, y = value, color = series)
+    ) + geom_line()
   })
   
   output$stock_volume_plot <- renderPlot({
-    
-    volume_data <- stock_data[[input$stock]] %>% 
-      filter(
-        between(index, input$dates[1], input$dates[2]),
-        grepl("Volume", series)
-      )
-      
-    ggplot(volume_data, aes(x = index, y = value, fill = series)) + geom_col()
+      ggplot(
+        log_filtered_data() %>% filter(group == "Volume"), 
+        aes(x = index, y = value, fill = series)
+      ) + geom_col()
   })
   
 }
